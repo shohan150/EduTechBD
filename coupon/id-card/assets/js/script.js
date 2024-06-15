d00259b079289609126df2cefedf7ec1031ba207
shohan <shohan.ece17@gmail.com>
2024-06-15 18:00:11 +0600

    beautyfy dynamic ID card

--- coupon/id-card/assets/js/script.js
+++ coupon/id-card/assets/js/script.js
@@ -13,30 +13,41 @@ form.addEventListener('submit', function(event) {
   const idNoLeft = parseInt(formData.get('idNoLeft'));
   const idNoSize = parseInt(formData.get('idNoSize'));
   const idNoWeight = parseInt(formData.get('idNoWeight'));
+  const idNoColor = formData.get('idNoColor');
   const nameTop = parseInt(formData.get('nameTop'));
   const nameLeft = parseInt(formData.get('nameLeft'));
   const nameSize = parseInt(formData.get('nameSize'));
   const nameWeight = parseInt(formData.get('nameWeight'));
+  const nameColor = formData.get('nameColor');
   const imageTop = parseInt(formData.get('imageTop'));
   const imageLeft = parseInt(formData.get('imageLeft'));
+  const imageSize = parseInt(formData.get('imageSize'));
   const borderWidth = parseInt(formData.get('borderWidth'));
   const borderRadius = parseInt(formData.get('borderRadius'));
+  const imageBorderColor = formData.get('imageBorderColor');
+
 
   // Update element styles based on form values
-  idNoElement.style.top = `${idNoTop}px`;
-  idNoElement.style.left = `${idNoLeft}px`;
+  idNoElement.style.top = `${idNoTop}%`;
+  idNoElement.style.left = `${idNoLeft}%`;
   idNoElement.style.fontSize  = `${idNoSize}px`;
-  idNoElement.style.fontWeight  = `${idNoWeight}px`;
+  idNoElement.style.fontWeight  = `${idNoWeight}`;
+  idNoElement.style.color  = idNoColor;
 
-  studentNameElement.style.top = `${nameTop}px`;
-  studentNameElement.style.left = `${nameLeft}px`;
+  studentNameElement.style.top = `${nameTop}%`;
+  studentNameElement.style.left = `${nameLeft}%`;
   studentNameElement.style.fontSize  = `${nameSize}px`;
-  studentNameElement.style.fontWeight  = `${nameWeight}px`;
+  studentNameElement.style.fontWeight  = `${nameWeight}`;
+  studentNameElement.style.color  = nameColor;
 
-  studentImageElement.style.top = `${imageTop}px`;
-  studentImageElement.style.left = `${imageLeft}px`;
+  studentImageElement.style.top = `${imageTop}%`;
+  studentImageElement.style.left = `${imageLeft}%`;
+  studentImageElement.style.height = `${imageSize}px`;
+  studentImageElement.style.width = `${imageSize}px`;
   studentImageElement.style.borderRadius  = `${borderRadius}px`;
   studentImageElement.style.borderWidth  = `${borderWidth}px`;
+  studentImageElement.style.borderColor  = imageBorderColor;
+
 });
 
 saveButton.addEventListener("click", (event)=>{
