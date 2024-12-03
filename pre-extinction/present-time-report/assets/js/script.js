0eafc3a88f0dfb360a30cfff7c727ab9b914c969
shohan <shohan.ece17@gmail.com>
2024-12-03 14:13:14 +0600

    fractional time added

--- pre-extinction/present-time-report/assets/js/script.js
+++ pre-extinction/present-time-report/assets/js/script.js
@@ -1,5 +1,5 @@
 const employeeReports = [
-   { name: "Jalil Raihan", report: [4, 1, 2, 2, 1] },
+   { name: "Jalil Raihan", report: [4.2, 0.2, 2, 2, 1] },
    { name: "Maya Khan", report: [3, 2, 3, 1, 1] },
    { name: "Rahim Ali", report: [5, 1, 2, 1, 1] },
    { name: "Sara Jahan", report: [2, 3, 2, 2, 1] },
@@ -19,14 +19,32 @@ employeeReports.forEach(employee => {
    const progressBar = document.createElement('div');
    progressBar.className = 'progress-bar';
 
+   let totalHours = 0;
+   let usedWidthPercentage = 0;
+   employee.report.forEach(hours => {
+       totalHours += hours;
+   });
+
    employee.report.forEach((hours, index) => {
-       for (let i = 0; i < hours; i++) {
-           const segment = document.createElement('div');
-           segment.className = 'progress-segment ' + (index % 2 === 0 ? 'present' : 'absent');
-           progressBar.appendChild(segment);
-       }
+    const ratio = Math.round((hours / totalHours) * 100);
+    const segment = document.createElement('div');
+    segment.className = 'progress-segment ' + (index % 2 === 0 ? 'present' : 'absent');
+    if(index === employee.report.length -1){
+      segment.style.width = `${100 - usedWidthPercentage}%`;
+    }else{
+      segment.style.width = `${ratio}%`;
+      usedWidthPercentage += ratio;
+    }
+    const timeText = getTimeText(hours);
+    segment.innerText = timeText;
+    progressBar.appendChild(segment);
    });
 
    employeeDiv.appendChild(progressBar);
    container.appendChild(employeeDiv);
 });
+
+function getTimeText(hours) {
+  const hoursText = hours === 1 ? 'hour' : 'hours';
+  return `${hours} ${hoursText}`;
+}
